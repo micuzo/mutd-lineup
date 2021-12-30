c8a3888433506414d91da4086be75c08f6524df1
Michael Uzoegwu <nouzoegwu@gmail.com>
2021-12-29 22:46:10 -0500

    Add arguments to main entrypoint
    Added two arguments to main.py:
      '-lineup': runs the main_exec function in lineup.py
      '-mulbot': runs the main_exec function in mulbot.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import tweepy
 import mulbot
+import lineup
+import sys
 from env import twitter_keys
 
 auth = tweepy.OAuthHandler(twitter_keys["API_KEY"], twitter_keys["API_KEY_SECRET"])
@@ -12,4 +14,15 @@ client = tweepy.Client(
     access_token_secret = twitter_keys["ACCESS_TOKEN_SECRET"]
 )
 
-mulbot.main_exec(client)
+args = sys.argv[1:]
+if len(args) == 0 or args[0] == '-mulbot':
+    mulbot.main_exec(client)
+elif args[0] == '-lineup':
+    lineup.main_exec()
+elif args[0] == '-h':
+    help_str = '''Options
+    -lineup   update information in out.json
+    -mulbot   check if lineup is out and tweet it'''
+    print(help_str)
+else:
+    print("Wrong paramters")
